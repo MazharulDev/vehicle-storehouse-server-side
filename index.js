7b6aa145b4638077a7437c47486f2d55cd64a0bf
Md Mazharul islam <miforbd@gmail.com>
2022-04-29 09:11:44 +0600

    post item added

--- index.js
+++ index.js
@@ -10,6 +10,33 @@ app.use(cors());
 app.use(express.json());
 
 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6sagz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const itemCollection=client.db("itemVehicle").collection("item");
+        //get item
+        app.get('/item',async(req,res)=>{
+            const query={};
+            const cursor=itemCollection.find(query);
+            const items=await cursor.toArray();
+            res.send(items);
+        })
+        //post item
+        app.post('/item', async(req,res)=>{
+            const newItem=req.body;
+            const result=await itemCollection.insertOne(newItem);
+            res.send(result)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir);
 
  
 app.get('/',(req,res)=>{
