1418c62f8223ea78557f43f8e30f9ab272e2720d
Md Mazharul islam <miforbd@gmail.com>
2022-05-01 08:02:22 +0600

    single item api created

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ async function run(){
             const result=await itemCollection.insertOne(newItem);
             res.send(result)
         })
-
+        // single api
         app.get('/item/:id', async(req,res)=>{
             const id=req.params.id;
             const query={_id: ObjectId(id)};
